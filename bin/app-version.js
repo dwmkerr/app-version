b8fd616f073eab615df5b1b170b28545abd46eec
Dave Kerr <dwmkerr@gmail.com>
2020-03-12 14:50:16 +0800

    chore: try to read provided platforms for set-version

--- bin/app-version.js
+++ bin/app-version.js
@@ -1,25 +1,27 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const chalk = require('chalk');
+// const chalk = require('chalk');
 const pack = require('../package.json');
 const showVersions = require('../src/commands/show-versions');
 const setVersion = require('../src/commands/set-version');
 
 //  Create the program, ensure that an 'action' is mandatory.
 program
-  .version(pack.version)
-  .option('-s, --search [optional]', 'The folder to search from.', './')
-  .option('-p, --platforms [optional]', 'The platforms to generate icons for.', 'android,ios');
+  .version(pack.version);
 
 program
   .command('show-versions')
   .description('shows versions')
+  .option('-s, --search-root [optional]', 'The folder to search from.', './')
+  .option('-p, --platforms [optional]', 'The platforms to show versions for.', 'android,ios')
   .action(showVersions);
 
 program
   .command('set-version <version>')
   .description('sets the version')
+  .option('-s, --search-root [optional]', 'The folder to search from.', './')
+  .option('-p, --platforms [optional]', 'The platforms to set versions for.', 'android,ios')
   .action(setVersion);
 
 //  Extend the help with some examples.
@@ -33,16 +35,12 @@ program.on('--help', () => {
 
 //  Show help for unknown commands.
 program.on('command:*', () => program.help());
-const main = async () => {
-  try {
-    //  Parse the arguments. If we have no subcommand, show the help.
-    await program.parseAsync(process.argv);
-    if (program.args.length === 0) {
-      program.help();
-    }
-  } catch (err) {
-    console.log(chalk.red(`Error running command: ${err.message}`));
-  }
-};
 
-main();
+//  Parse the arguments. If we have no subcommand, show the help.
+program.parse(process.argv);
+
+// Note: this doesn't work, because if we run 'show-versions', then the args length
+// is zero.
+// if (program.args.length === 0) {
+//   program.help();
+// }
--- src/commands/set-version.js
+++ src/commands/set-version.js
@@ -8,23 +8,27 @@ const editGradleFile = require('../android/edit-gradle-file');
 module.exports = async function setVersion(version, parameters) {
   const {
     searchRoot,
-    // platforms,
+    platforms,
   } = getGlobalParameters(parameters);
   try {
-    const plistFiles = await findPlistFiles(searchRoot);
-    plistFiles.forEach(async (plistFile) => {
-      await editPlistFile(plistFile, [{
-        key: /CFBundleShortVersionString/,
-        value: version,
-      }]);
-      console.log(`${chalk.green('ios')} ${plistFile}: CFBundleShortVersionString set to ${version}`);
-    });
+    if (platforms.includes('ios')) {
+      const plistFiles = await findPlistFiles(searchRoot);
+      plistFiles.forEach(async (plistFile) => {
+        await editPlistFile(plistFile, [{
+          key: /CFBundleShortVersionString/,
+          value: version,
+        }]);
+        console.log(`${chalk.green('ios')} ${plistFile}: CFBundleShortVersionString set to ${version}`);
+      });
+    }
 
-    const gradleFiles = await findGradleFiles(searchRoot);
-    gradleFiles.forEach(async (gradleFile) => {
-      const updated = await editGradleFile(gradleFile, /versionName\s+"[\d.]+"/, `versionName "${version}"`);
-      if (updated) console.log(`${chalk.green('android')} ${gradleFile}: versionCode set to ${version}`);
-    });
+    if (platforms.includes('android')) {
+      const gradleFiles = await findGradleFiles(searchRoot);
+      gradleFiles.forEach(async (gradleFile) => {
+        const updated = await editGradleFile(gradleFile, /versionName\s+"[\d.]+"/, `versionName "${version}"`);
+        if (updated) console.log(`${chalk.green('android')} ${gradleFile}: versionCode set to ${version}`);
+      });
+    }
   } catch (err) {
     console.error(chalk.red(`Error setting version: ${err.message}`));
     process.exit(1);
